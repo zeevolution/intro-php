e4d65c4b89e7d095fb2a1c371204c8261ffc75ae
zeevolution <limajosent@gmail.com>
2022-04-28 18:59:51 -0300

    add fizzbuzz copy

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,17 +1,18 @@
 <?php
 
     function fizzbuzz($number) { 
-        for($currentNumber = 0; $currentNumber <= $number; $currentNumber++) {
-            if ($currentNumber % 3 === 0 && $currentNumber % 5 === 0) {
-                echo "fizzbuzz"."<br />";
-            } elseif ($currentNumber % 3 === 0) {
-                echo "fizz";
-            } elseif ($currentNumber % 5 === 0) {
-                echo "buzz"."<br />";
-            } else {
-                echo $currentNumber;
-            }
-        }
+
+        // Loop de 0 a $number
+
+            // Dentro do loop, validar se:
+
+            // É mutliplos de 3 e 5
+
+            // É multiplo de 3
+
+            // É multiplo de 5
+
+            // Ou é nenhum dos casos acim
     }
 
     fizzbuzz(5);
