49671524d69a6e036faada65d56c4c8233735054
zeevolution <limajosent@gmail.com>
2022-04-28 20:39:26 -0300

    add fizzbuzz description

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,5 +1,18 @@
 <?php
 
+    // Escreva um programa PHP que:
+    // exibe números 1 a n,
+    // mas para múltiplos de três o programa exibe o texto "fizz" em vez do número
+    // e para múltiplos de cinco imprime "buzz".
+    // Por fim, para números que são múltiplos de três e cinco imprime "fizzbuzz"
+    //
+    // Exemplo:
+    // fizzBuzz(5);
+    // 1
+    // 2
+    // fizz
+    // 4
+    // buzz
     function fizzbuzz($number) { 
 
         // Loop de 0 a $number
